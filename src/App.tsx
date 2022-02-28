21478d590d28ff8a325bdabc92b8360c5b2a4111
Elia Lazzari <elia.lazzari@gmail.com>
2022-02-28 11:06:18 +0100

    Fixed bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Home from "./onepirate/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
